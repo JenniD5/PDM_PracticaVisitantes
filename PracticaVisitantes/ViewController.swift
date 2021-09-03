91181cda57ef9d89b2d0b265fc51d4acca9aa8e6
Araceli Arizmendi <araceli.muse@gmail.com>
2021-09-03 14:21:47 -0700

    Initial Commit

---
+++ PracticaVisitantes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PracticaVisitantes
+//
+//  Created by Alumno on 9/3/21.
+//  Copyright © 2021 Alumno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
